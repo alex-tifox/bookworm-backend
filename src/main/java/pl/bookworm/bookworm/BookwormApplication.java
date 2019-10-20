e0f2c69fd972272baee6a4f02b3116de75d9a361
alex.tifox <tilnyi.olexander@gmail.com>
2019-10-20 23:56:43 +0200

    #BW-12 excluding Spring Security module

--- src/main/java/pl/bookworm/bookworm/BookwormApplication.java
+++ src/main/java/pl/bookworm/bookworm/BookwormApplication.java
@@ -2,8 +2,9 @@ package pl.bookworm.bookworm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BookwormApplication {
 
 	public static void main(String[] args) {
