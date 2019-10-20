50d6accdda7fd62ad18b1d08dee0dc620846f095
alex.tifox <tilnyi.olexander@gmail.com>
2019-10-20 23:55:07 +0200

    #BW-12 sample of getting books by giving author's name

--- src/main/java/pl/bookworm/bookworm/controller/Controller.java
+++ src/main/java/pl/bookworm/bookworm/controller/Controller.java
@@ -1,16 +1,27 @@
 package pl.bookworm.bookworm.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.books.model.Volumes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import pl.bookworm.bookworm.service.ServiceName;
+import pl.bookworm.bookworm.service.BooksApi;
 
 @RestController
 public class Controller {
 
-    private ServiceName serviceName;
+    @GetMapping("/getAnyBook/{query}")
+    public Volumes getAnyBook(@PathVariable("query") String query) {
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        query = "inauthor:" + query;
+        Volumes volumes = new Volumes();
+        try {
+            volumes = BooksApi.queryGoogleBooks(jsonFactory, query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    @Autowired
-    public Controller(ServiceName serviceName) {
-        this.serviceName = serviceName;
+        return volumes;
     }
 }
