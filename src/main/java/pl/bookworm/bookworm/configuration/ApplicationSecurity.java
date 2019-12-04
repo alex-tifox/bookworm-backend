b09123c2ea19049eaef90c90ef32d92c0c7673b7
Mateusz <kolmat97>
2019-12-04 18:30:47 +0100

    Access restriciton by roles
    Created security adapter and added basic access controll

---
+++ src/main/java/pl/bookworm/bookworm/configuration/ApplicationSecurity.java
@@ -0,0 +1,27 @@
+package pl.bookworm.bookworm.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurity  extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin/**")
+				.hasRole("ADMIN")
+			.antMatchers("/user/**")
+				.hasRole("USER")
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+}
