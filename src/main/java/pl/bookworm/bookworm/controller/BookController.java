19949f05d101a199a6fe43a48e1b0d36688a02ee
alex.tifox <tilnyi.olexander@gmail.com>
2019-12-07 12:28:06 +0100

    #BW-39 endpoints from task were implemented

--- src/main/java/pl/bookworm/bookworm/controller/BookController.java
+++ src/main/java/pl/bookworm/bookworm/controller/BookController.java
@@ -4,12 +4,17 @@ import com.google.api.services.books.model.Volumes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.bookworm.bookworm.model.Book;
+import pl.bookworm.bookworm.model.BookReview;
 import pl.bookworm.bookworm.repository.BookRepository;
 import pl.bookworm.bookworm.service.BookService;
 
+import java.util.Set;
+
 @RestController
+@RequestMapping("books")
 public class BookController {
 
     private BookService bookService;
@@ -33,4 +38,21 @@ public class BookController {
         return bookService.getBooksByBookName(bookName);
     }
 
+    // TODO: move logic of getting book's info to service
+    @GetMapping("/getBookInfo/{id}")
+    public Book getBookInfo(@PathVariable("id") Long id) {
+        return bookRepository.findById(id).orElse(new Book());
+    }
+
+    @GetMapping("/getAllBookReviews/{id}")
+    public Set<BookReview> getAllBookReviews(@PathVariable("id") Long id) {
+        Book requiredBook = bookRepository.findById(id).orElse(new Book());
+        return requiredBook.getBookReviews();
+    }
+
+    @GetMapping("/getBookRate/{id}")
+    public Double getBookRate(@PathVariable("id") Long id) {
+        Book requiredBook = bookRepository.findById(id).orElse(new Book());
+        return requiredBook.getBookAverageRate();
+    }
 }
