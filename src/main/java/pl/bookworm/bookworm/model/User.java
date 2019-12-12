48948754aca0e8436d256ca80df40ef8aa4954fe
alex.tifox <tilnyi.olexander@gmail.com>
2019-12-12 15:24:12 +0100

    #BW-50 Entities rewritten to lombok syntax

--- src/main/java/pl/bookworm/bookworm/model/User.java
+++ src/main/java/pl/bookworm/bookworm/model/User.java
@@ -1,8 +1,15 @@
 package pl.bookworm.bookworm.model;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@Builder(toBuilder = true)
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 @Table(name = "users")
 public class User {
@@ -10,84 +17,12 @@ public class User {
     //TODO: on production - change generation type to sequence
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    Long id;
 
-    @Column(name = "USERNAME")
-    private String username;
-
-    @Column(name = "PASSWORD")
-    private String password;
-
-    @Column(name = "USER_FULLNAME")
-    private String userFullname;
-
-    @Column(name = "EMAIL")
-    private String email;
-
-    @Column(name = "REGISTRATION_DATE")
-    private Date registrationDate;
-
-    @Column(name = "LAST_LOGIN_DATE")
-    private Date lastLoginDate;
-
-    public User() {}
-    public User(String username) {
-        this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserFullname() {
-        return userFullname;
-    }
-
-    public void setUserFullname(String userFullname) {
-        this.userFullname = userFullname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public Date getLastLoginDate() {
-        return lastLoginDate;
-    }
-
-    public void setLastLoginDate(Date lastLoginDate) {
-        this.lastLoginDate = lastLoginDate;
-    }
+    String username;
+    String password;
+    String userFullname;
+    String email;
+    Date registrationDate;
+    Date lastLoginDate;
 }
