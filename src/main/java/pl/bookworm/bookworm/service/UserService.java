fdb40d4e6c07a0286e4f460bca76957f6743bee6
alex.tifox <tilnyi.olexander@gmail.com>
2019-12-12 15:26:25 +0100

    #BW-50 Services for user rewritten for lombok features support

--- src/main/java/pl/bookworm/bookworm/service/UserService.java
+++ src/main/java/pl/bookworm/bookworm/service/UserService.java
@@ -1,34 +1,29 @@
 package pl.bookworm.bookworm.service;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import pl.bookworm.bookworm.model.User;
 import pl.bookworm.bookworm.repository.UserRepository;
 
-import java.util.logging.Logger;
-
 @Service
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class UserService {
-
-	final private Logger logger = Logger.getLogger(UserService.class.getName());
-
 	final private UserRepository userRepository;
 	final private PasswordEncoder passwordEncoder;
 	final private UserDetailsServiceImpl userDetailsService;
 
-	@Autowired
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserDetailsServiceImpl userDetailsService) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.userDetailsService = userDetailsService;
-	}
-
 	public boolean registerUser(User newUser){
     	if (userRepository.findByUsername(newUser.getUsername()) != null) {
-    		logger.info("User is already existing");
+    		log.info("User is already existing");
 			return false;
 		}
     	newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
@@ -39,15 +34,18 @@ public class UserService {
     public User loginUser(User user) {
 		if (checkUserPassword(user))
 			return getLoggedInUser(user.getUsername());
-		else
-			return new User("");
+		else {
+			return User.builder()
+					.username("")
+					.build();
+		}
 	}
 
     private boolean checkUserPassword(User user) {
 		UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
 
-		logger.info("Raw password: " + user.getPassword());
-		logger.info("Password in db: " + userDetails.getPassword());
+		log.info("Raw password: " + user.getPassword());
+		log.info("Password in db: " + userDetails.getPassword());
 
 		return passwordEncoder.matches(user.getPassword(), userDetails.getPassword());
 	}
