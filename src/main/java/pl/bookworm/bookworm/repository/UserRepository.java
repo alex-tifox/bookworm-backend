08489d00f1b68c1a6024c82ac5492bd9efd7e943
Mateusz <kolmat97>
2019-12-14 20:42:56 +0100

    #BW-61 Request for geting users by email added

--- src/main/java/pl/bookworm/bookworm/repository/UserRepository.java
+++ src/main/java/pl/bookworm/bookworm/repository/UserRepository.java
@@ -7,4 +7,5 @@ import pl.bookworm.bookworm.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByEmail(String email);
 }
