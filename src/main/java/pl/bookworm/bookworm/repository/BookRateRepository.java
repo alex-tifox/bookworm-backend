7c425425a4fd8ff4a547149dac7748f611bf1a4a
Mateusz <kolmat97>
2020-01-16 00:25:16 +0100

    #BW-86 Repository for book ratings

---
+++ src/main/java/pl/bookworm/bookworm/repository/BookRateRepository.java
@@ -0,0 +1,17 @@
+package pl.bookworm.bookworm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import pl.bookworm.bookworm.model.Book;
+import pl.bookworm.bookworm.model.BookRate;
+import pl.bookworm.bookworm.model.User;
+
+@Repository
+public interface BookRateRepository extends JpaRepository<BookRate, Long> {
+	BookRate findByRateAuthorAndRatedBook(User user, Book book);
+	
+	@Query("Select avg(r.rate) from BookRate r where r.ratedBook = ?1")
+	double getAverageRatingByBook(Book ratedBook);
+}
