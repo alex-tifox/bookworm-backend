56fa89845bd2329fbc704592de152de36a1e5d94
alex.tifox <tilnyi.olexander@gmail.com>
2020-01-26 19:55:06 +0100

    #BW-63 repository method for looking books in local database customized

--- src/main/java/pl/bookworm/bookworm/repository/BookRepository.java
+++ src/main/java/pl/bookworm/bookworm/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package pl.bookworm.bookworm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.bookworm.bookworm.model.Book;
 
+import java.util.Set;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query(value = "select * from books b where b.title like %?1%", nativeQuery = true)
+    Set<Book> findBooksByTitle(String titleQuery);
 }
