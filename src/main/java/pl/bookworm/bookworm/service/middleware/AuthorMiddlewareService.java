ea384aaab98e5f11bf65e0bfc606c06cf052d0b7
alex.tifox <tilnyi.olexander@gmail.com>
2020-01-27 20:20:32 +0100

    #BW-63 scratch for author middleware

---
+++ src/main/java/pl/bookworm/bookworm/service/middleware/AuthorMiddlewareService.java
@@ -0,0 +1,16 @@
+package pl.bookworm.bookworm.service.middleware;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Service
+public class AuthorMiddlewareService {
+
+
+}
