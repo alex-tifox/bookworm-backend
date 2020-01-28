7b342651945211c9505f5ffe7c9dffab16566d5e
Mateusz <kolmat97>
2020-01-28 15:42:51 +0100

    #BW-63 Changed search query, updated methods to changed query return type

--- src/main/java/pl/bookworm/bookworm/service/middleware/BookMiddlewareService.java
+++ src/main/java/pl/bookworm/bookworm/service/middleware/BookMiddlewareService.java
@@ -4,11 +4,17 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+
+import org.hibernate.dialect.pagination.FirstLimitHandler;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+
+import pl.bookworm.bookworm.model.Author;
 import pl.bookworm.bookworm.model.Book;
 import pl.bookworm.bookworm.repository.BookRepository;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -22,40 +28,42 @@ public class BookMiddlewareService {
     BooksApiQuery booksApi;
 
     public Set<Book> getAuthorBooks(String query) {
-        return booksApi.getBooks(query, true);
+        booksApi.getBooks(query, true);
+        return new HashSet<>();
     }
 
     public Set<Book> getBooksByName(String query) {
         // check book existing in repo - if no existing - go to google and save it in our db and return to the user
         // otherwise - return what you've found in db
-       Set<Book> books = bookRepository.findBooksByTitle(query);
+       Set<Book> books = bookRepository.findBooksByTitleContainingIgnoreCase(query);
        log.info(books.toString());
+       
        if (books.isEmpty()) {
            log.info("books is empty, going to Google API");
            // Go to google api: get , save and return
-           books = booksApi.getBooks(query, false);
+           Set<Pair<Book, Author>> booksWithAuthors = booksApi.getBooks(query, false);
 
            log.warn("Books for saving");
            log.info(books.toString());
 
-           saveAllBooks(books);
+           saveAllBooks(booksWithAuthors);
 
-           return bookRepository.findBooksByTitle(query);
+           return bookRepository.findBooksByTitleContainingIgnoreCase(query);
        }
        return books;
     }
 
-    private void saveAllBooks(Set<Book> booksToSave) {
-        for (Book book: booksToSave) {
+    private void saveAllBooks(Set<Pair<Book, Author>> booksWithAuthorsToSave) {
+        for (Pair<Book, Author> bookWithAuthor: booksWithAuthorsToSave) {
             bookRepository.insertBook(
-                    book.getAuthorName(),
-                    book.getPublicationYear(),
-                    book.getTitle(),
-                    book.getIsbn(),
-                    book.getDescription(),
-                    book.getThumbnailUrl(),
-                    book.getCategories(),
-                    book.getGoogleApiId());
+                    bookWithAuthor.getSecond().getName(),
+                    bookWithAuthor.getFirst().getPublicationYear(),
+                    bookWithAuthor.getFirst().getTitle(),
+                    bookWithAuthor.getFirst().getIsbn(),
+                    bookWithAuthor.getFirst().getDescription(),
+                    bookWithAuthor.getFirst().getThumbnailUrl(),
+                    bookWithAuthor.getFirst().getCategories(),
+                    bookWithAuthor.getFirst().getGoogleApiId());
         }
     }
 }
