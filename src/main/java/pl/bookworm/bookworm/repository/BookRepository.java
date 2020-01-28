d13da20b50b1072fc612dcdced08013f51299250
Mateusz <kolmat97>
2020-01-28 15:39:28 +0100

    #BW-63 Change native query to jpa query and made like search case invariant

--- src/main/java/pl/bookworm/bookworm/repository/BookRepository.java
+++ src/main/java/pl/bookworm/bookworm/repository/BookRepository.java
@@ -1,20 +1,16 @@
 package pl.bookworm.bookworm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.bookworm.bookworm.model.Book;
-
 import java.util.Set;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-    @Query(value = "select * from books where title like %:title%", nativeQuery = true)
-    Set<Book> findBooksByTitle(@Param("title")String titleQuery);
-
+    Set<Book> findBooksByTitleContainingIgnoreCase(String titleQuery);
+    
     @Procedure(procedureName = "insert_books")
     void insertBook(@Param("AuthorName") String authorName,
                     @Param("PublicationYear") Integer publicationYear,
